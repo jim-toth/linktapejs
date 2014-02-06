cff4c99ae71c8acf4fc88a73896498defc76d2fd
jim-toth <gordon.toth@gmail.com>
2014-02-06 00:15:04 -0500

    Player controls.
    Current song play status updates are not being reflected in the UI.
    
    Also loading spinners.

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,33 @@
 var linktapeControllers = angular.module('linktapeControllers', []);
 
 linktapeControllers.controller('PlaylistCtrl', ['$scope', 'Playlist', function ($scope, Playlist) {
-    $scope.playlist_data = Playlist.get({ pid: '2fcktcp0w' }, function (playlist_data) {
+	$scope.playlist_data = Playlist.get({ pid: '2fcktcp0w' }, function (playlist_data) {
 		$scope.pid = playlist_data.pid;
 		$scope.title = playlist_data.name;
 		$scope.playlist = playlist_data.playlist;
-    });
+		$scope.currentSong = undefined;
+	});
+
+	$scope.songPlaying = {};
+	$scope.songPlaying.isPlaying = false;
+
+	$scope.setSongPlaying = function (isPlaying) {
+		$scope.songPlaying.isPlaying = isPlaying;
+	}
+
+	$scope.toggle = function () {
+		if(typeof $scope.currentSong == 'undefined') {
+			$scope.currentSong = $scope.playlist[0];
+		}
+
+		$scope.currentSong.toggle();
+	}
+
+	$scope.prev = function () {
+		console.log('prev');
+	}
+
+	$scope.next = function () {
+		console.log('next');
+	}
 }]);
--- js/directives.js
+++ js/directives.js
@@ -1,19 +1,56 @@
 var linktapeSong = angular.module('linktapeSong', ['linktapeServices']);
 
 linktapeSong.directive('lsSong', ['ResolveURI', function (ResolveURI) {
-	var SCOPTS = {
+	SCOPTS = {
 		"auto_play" : false,
 		"show_comments" : false,
 		"iframe" : true
 	};
 
 	function link($scope, $element, $attrs) {
+		var $playlistScope = $scope.$parent.$parent;
 		if($scope.song.song_type == 'sc') {
 			ResolveURI.get({ uri: $scope.song.uri }, function (uri_data) {
 				SC.get('/resolve', { url: uri_data.resolvedURL }, function (track) {
-					SC.oEmbed('http://api.soundcloud.com/tracks/'+track.id, SCOPTS, function (oEmbed) {
-						$element.find('div').replaceWith(oEmbed.html);
-					});
+					if(track.embeddable_by == 'all') {
+						$scope.song.track = track;
+						SC.oEmbed('http://api.soundcloud.com/tracks/' + track.id, SCOPTS, function (oEmbed) {
+							
+							$element.find('div').replaceWith(oEmbed.html);
+							$scope.song.player = SC.Widget($element.find('iframe')[0]);
+
+							$scope.song.player.bind(SC.Widget.Events.PLAY, function (ev) {
+								if ($playlistScope.currentSong != 'undefined') {
+									if($playlistScope.currentSong !== $scope.song) {
+										$playlistScope.currentSong.stop();
+										$playlistScope.currentSong = $scope.song;
+									}
+								}
+
+								$playlistScope.setSongPlaying(true);
+							});
+
+							$scope.song.player.bind(SC.Widget.Events.PAUSE, function () {
+								if ($playlistScope.currentSong != 'undefined') {
+									if($playlistScope.currentSong === $scope.song) {
+										$playlistScope.setSongPlaying(false);
+									}
+								}
+							});
+							
+							$scope.song.toggle = function () {
+								$scope.song.player.toggle();
+							}
+
+							$scope.song.stop = function () {
+								$scope.song.player.pause();
+								$scope.song.seek(0);
+							}
+
+						});
+					} else {
+						// TODO: notify user of restriction
+					}
 				});
 			});
 		}
@@ -26,5 +63,15 @@ linktapeSong.directive('lsSong', ['ResolveURI', function (ResolveURI) {
 		},
 		templateUrl: 'js/directives/ls-song.html',
 		link: link
+	};
+}]);
+
+linktapeSong.directive('lsControls', [function () {
+	return {
+		restrict: 'E',
+		scope: {
+			playlist: '='
+		},
+		templateUrl: 'js/directives/ls-controls.html'
 	};
 }]);
